82bf004eb512a6d8abceffdec728a2518dc9de37
Nikita Vasilev <vasilevnikitad@gmail.com>
2020-03-31 11:26:41 +0300

    opengl_window.cpp: add simple drawing

--- src/opengl_window.cpp
+++ src/opengl_window.cpp
@@ -55,6 +55,8 @@ int main(int, char *[])
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
     }
 
